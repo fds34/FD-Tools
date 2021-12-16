4132b28a6e0f00da4c3aa643c936a0b931cfc5d8
ifdu4550 <francois.dunoyer@pole-emploi.fr>
2021-12-16 14:19:54 +0100

    ajout config pour desactiver le lancement du server manager

---
+++ Set-StartServerManager.ps1
@@ -0,0 +1,23 @@
+﻿<#
+ .SYNOPSIS
+    Désactiver le lancement du Gestionnaire de Serveurs ( Server Manager)
+ .Description 
+    Désactiver le lancement du Gestionnaire de Serveurs ( Server Manager) au lancement d'un serveur
+ .NOTES
+    C'est aussi possible de faire par GPO
+    On va ici faire la configuration pour l'utilisateur courant.
+    La configuration n'est poussé que si c'est nécessaire
+#>
+
+# Désactiver le lancement du gestionnaire de serveurs
+try { $Res=get-ItemProperty -Path HKCU:\Software\Microsoft\ServerManager -Name DoNotOpenServerManagerAtLogon -ErrorAction SilentlyContinue} catch {$res=$Null}
+$Sb={
+    $Message='Désactiver le lancement du gestionnaire de serveurs'
+    Write-Host $Message -ForegroundColor Yellow
+    New-ItemProperty -Path HKCU:\Software\Microsoft\ServerManager -Name DoNotOpenServerManagerAtLogon -PropertyType DWORD -Value "0x1" –Force
+    $Res=get-ItemProperty -Path HKCU:\Software\Microsoft\ServerManager -Name DoNotOpenServerManagerAtLogon
+}
+if ($res.DoNotOpenServerManagerAtLogon -ne 1) { & $SB } else {
+    $Message='Lancement du gestionnaire de serveurs : Désactivé'
+    Write-Host $Message -ForegroundColor green
+}
